f5a06c5ce18deba9253c3436b5e95897edc0e4eb
PaolaGaribaldi <151163495+PaolaGaribaldi@users.noreply.github.com>
2023-12-08 09:15:02 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ addEventListener('scroll',(function(){
     let pages = Math.round(this.scrollY/this.window.innerHeight)
     this.document.getElementById('pages_indicator').innerText = '0'+pages;
     this.document.getElementById('chbck'+pages).style.opacity = '100%';
-    for (let i = 0; i < 3; i++){
+    for (let i = 0; i < 4; i++){
       if(i != pages){
         this.document.getElementById('chbck'+i).style.opacity = '30%';
       }
@@ -11,3 +11,9 @@ addEventListener('scroll',(function(){
         this.document.getElementById('moving').style.transform = 'rotate(180deg) translateX(3vw) translateY('+((this.scrollY/8)-5) +'vh)';
     }
 }))
+addEventListener('mousemove',(event) => {
+    this.document.getElementById('tab').style.transform = 'translate('+((this.event.clientX)-(window.innerWidth/2))/30+'px,'+((this.event.clientY)-(window.innerHeight/2))/30+'px)';
+    this.document.getElementById('tab2').style.transform = 'translate('+((this.event.clientX)-(window.innerWidth/2))/30+'px,'+((this.event.clientY)-(window.innerHeight/2))/30+'px)';
+    console.log('translate('+((this.event.clientX)-(window.innerWidth/2))/20+'px,'+((this.event.clientY)-(window.innerHeight/2))/20+'px)')
+    console.log(((this.event.clientX)-(window.innerWidth/2))/30+'px,'+((this.event.clientY)-(window.innerHeight/2))/30+'px)');
+})
